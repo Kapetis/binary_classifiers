fec9e34af0f4d7e30890b9e7f7a62a849a47ebf5
Johannes Bauer <johannes.bauer@sclgroup.cc>
2015-09-29 14:37:57 +0100

    Added option which model to run and output files

--- binaryclassifier.py
+++ binaryclassifier.py
@@ -63,6 +63,8 @@ from sklearn import svm
 # Global variable
 metrics_dict={}
 timing_dict={}
+modelparameter_dict={}
+# entries are dictionaries of parameters for each model
 
 def npmat_to_rdd_wreadwrite(sc,X,Y,f_name,delete_file=False):
     """
@@ -189,11 +191,17 @@ if __name__ == "__main__":
     run_scikit = True
     run_spark_mllib = True
 
-    # Output
+
+    # List of models used to run
+    model_list=['Tree_scikit','RF_scikit','GBT_scikit','logreg_scikit','SVM_scikit','Tree_mllib','RF_mllib','GBT_mllib','logreg_mllib','SVM_mllib']
+
+    # Write output to files
     write_dict2file = False
+
     format_json = False
     format_csv = True
-    # could be more specific here, possibly include timestamp
+
+    # This name could be more specific here
     outfilename='results'
 
     ###############################################################################
@@ -220,70 +228,115 @@ if __name__ == "__main__":
       # Scikit learn models
       ###############################################################################
       # Simple tree
-      t0=datetime.datetime.now()
-      model_scikit_tree=tree.DecisionTreeClassifier(criterion='gini', max_depth=5)
+      if 'Tree_scikit' in model_list:
+        t0=datetime.datetime.now()
 
-      model_scikit_tree.fit(X_train,Y_train)
+        # Only change parameters here
+        tree_scikit_par_dict={}
+        #tree_scikit_par_dict['numClasses'] = 2
+        tree_scikit_par_dict['criterion'] = 'gini'
+        tree_scikit_par_dict['max_depth'] = 2
 
-      scikit_model_accuracy('Tree',model_scikit_tree,X_train,Y_train,X_test,Y_test)
+        model_scikit_tree=tree.DecisionTreeClassifier(criterion=tree_scikit_par_dict['criterion'], max_depth=tree_scikit_par_dict['max_depth'])
 
-      runtime_write('Tree_scikit',t0)
+        model_scikit_tree.fit(X_train,Y_train)
+
+        scikit_model_accuracy('Tree',model_scikit_tree,X_train,Y_train,X_test,Y_test)
+
+        modelparameter_dict['Tree_scikit']=tree_scikit_par_dict
+
+        runtime_write('Tree_scikit',t0)    
+
+
+      ###############################################################################
       ###############################################################################
       # Random forest
-      t0=datetime.datetime.now()
-      model_scikit_RFT=RandomForestClassifier(max_depth=5,n_estimators=10,
-            criterion='gini')
+      if 'RF_scikit' in model_list:
+        t0=datetime.datetime.now()
 
-      model_scikit_RFT.fit(X_train,Y_train)
+        # Only change parameters here
+        RF_scikit_par_dict={}
+        RF_scikit_par_dict['criterion'] = 'gini'
+        RF_scikit_par_dict['max_depth'] = 10
+        RF_scikit_par_dict['n_estimator'] = 80
+        RF_scikit_par_dict['max_features'] = None
+        #RF_scikit_par_dict['max_features'] = 'sqrt'
+        #RF_scikit_par_dict['bootstrap'] = False
+        RF_scikit_par_dict['bootstrap'] = True
+        RF_scikit_par_dict['n_jobs'] = 4 
+        # currently not used
 
-      scikit_model_accuracy('RF',model_scikit_RFT,X_train,Y_train,X_test,Y_test)
+        model_scikit_RF=RandomForestClassifier(max_depth=RF_scikit_par_dict['max_depth'], max_features=RF_scikit_par_dict['max_features'],
+                                                bootstrap=RF_scikit_par_dict['bootstrap'], n_jobs=RF_scikit_par_dict['n_jobs'],
+                                                n_estimators=RF_scikit_par_dict['n_estimator'], criterion=RF_scikit_par_dict['criterion'])
 
-      runtime_write('RF_scikit',t0)
+        model_scikit_RF.fit(X_train,Y_train)
+
+        scikit_model_accuracy('RF',model_scikit_RF,X_train,Y_train,X_test,Y_test)
+
+        modelparameter_dict['RF_scikit']=RF_scikit_par_dict
+
+        runtime_write('RF_scikit',t0)
       ###############################################################################
       # Gradient boosted trees
-      t0=datetime.datetime.now()
-      model_scikit_GBT=GradientBoostingClassifier(
-            loss='deviance', learning_rate=0.1, n_estimators=10,
-            subsample=1, min_samples_split=5, min_samples_leaf=5, max_depth=5,
-            init=None, random_state=1, verbose=0,
-            max_leaf_nodes=None, warm_start=False)
+      if 'GBT_scikit' in model_list:
+        t0=datetime.datetime.now()
 
-#      model_scikit_GBT=GradientBoostingClassifier(
-#            loss='deviance', learning_rate=0.1, n_estimators=10,
-#            max_depth=5)
+        # Only change parameters here
+        GBT_scikit_par_dict={}
+        GBT_scikit_par_dict['loss'] = 'deviance'
+        GBT_scikit_par_dict['max_depth'] = 10
+        GBT_scikit_par_dict['n_estimator'] = 40
+        GBT_scikit_par_dict['learning_rate']=0.1
+        GBT_scikit_par_dict['subsample']=1
+        GBT_scikit_par_dict['min_samples_split']=5
+        GBT_scikit_par_dict['min_samples_leaf']=5
 
-      model_scikit_GBT.fit(X_train,Y_train)
+        model_scikit_GBT=GradientBoostingClassifier(
+            loss=GBT_scikit_par_dict['loss'], learning_rate=GBT_scikit_par_dict['learning_rate'], n_estimators=GBT_scikit_par_dict['n_estimator'],
+            subsample=GBT_scikit_par_dict['subsample'], min_samples_split=GBT_scikit_par_dict['min_samples_split'], 
+             min_samples_leaf=GBT_scikit_par_dict['min_samples_leaf'], max_depth=GBT_scikit_par_dict['max_depth'],
+            init=None, random_state=1, verbose=0,  max_leaf_nodes=None, warm_start=False)
 
-      scikit_model_accuracy('GBT',model_scikit_GBT,X_train,Y_train,X_test,Y_test)
+ 
+        model_scikit_GBT.fit(X_train,Y_train)
 
-      runtime_write('GBT_scikit',t0)
+        scikit_model_accuracy('GBT',model_scikit_GBT,X_train,Y_train,X_test,Y_test)
+
+        modelparameter_dict['GBT_scikit']=GBT_scikit_par_dict
+
+        runtime_write('GBT_scikit',t0)
+
       ###############################################################################
       # Logistic regression
-      t0=datetime.datetime.now()
-      #LogisticRegression(penalty='l2', dual=False, tol=0.0001, C=1.0, fit_intercept=True, intercept_scaling=1, 
-      #class_weight=None, random_state=None, solver='liblinear', max_iter=100, multi_class='ovr', verbose=0)
+      if 'logreg_scikit' in model_list:
+        t0=datetime.datetime.now()
+        #LogisticRegression(penalty='l2', dual=False, tol=0.0001, C=1.0, fit_intercept=True, intercept_scaling=1, 
+        #class_weight=None, random_state=None, solver='liblinear', max_iter=100, multi_class='ovr', verbose=0)
 
-      #model_scikit_logreg=LogisticRegression(penalty='l2', tol=0.0001, C=1.0, solver='liblinear', max_iter=100, multi_class='ovr')
-      model_scikit_logreg=LogisticRegression(penalty='l2', tol=0.0001, C=1.0, solver='lbfgs', max_iter=100, multi_class='ovr')
+        #model_scikit_logreg=LogisticRegression(penalty='l2', tol=0.0001, C=1.0, solver='liblinear', max_iter=100, multi_class='ovr')
+        model_scikit_logreg=LogisticRegression(penalty='l2', tol=0.0001, C=1.0, solver='lbfgs', max_iter=100, multi_class='ovr')
 
-      model_scikit_logreg.fit(X_train,Y_train)
+        model_scikit_logreg.fit(X_train,Y_train)
 
-      scikit_model_accuracy('logreg',model_scikit_logreg,X_train,Y_train,X_test,Y_test)
+        scikit_model_accuracy('logreg',model_scikit_logreg,X_train,Y_train,X_test,Y_test)
 
-      runtime_write('logreg_scikit',t0)
+        runtime_write('logreg_scikit',t0)
 
       ###############################################################################
       # Support vector machines (SVM)
-      t0=datetime.datetime.now()
-      #model_scikit_SVM =svm.SVC(C=1.0, cache_size=200, class_weight=None, coef0=0.0, degree=3, gamma=0.0,
-      #                      kernel='rbf', max_iter=-1, probability=False, random_state=None,
-      #                      shrinking=True, tol=0.001, verbose=False)
+      if 'SVM_scikit' in model_list:
+        t0=datetime.datetime.now()
+        model_scikit_SVM =svm.SVC(C=1.0, cache_size=200, class_weight=None, coef0=0.0, degree=3, gamma=0.0,
+                              kernel='rbf', max_iter=-1, probability=False, random_state=None,
+                              shrinking=True, tol=0.001, verbose=False)
 
-      #model_scikit_SVM.fit(X_train,Y_train)
+        model_scikit_SVM.fit(X_train,Y_train)
 
-      #scikit_model_accuracy('SVM',model_scikit_SVM,X_train,Y_train,X_test,Y_test)
+        scikit_model_accuracy('SVM',model_scikit_SVM,X_train,Y_train,X_test,Y_test)
 
-      runtime_write('SVM_scikit',t0)
+        runtime_write('SVM_scikit',t0)
+
 
     ###############################################################################
 
@@ -303,71 +356,103 @@ if __name__ == "__main__":
       trainingData_rdd.cache()
 
       ###############################################################################
-      # Simple decision tree
-      #  Empty categoricalFeaturesInfo indicates all features are continuous.
-      t0=datetime.datetime.now()
-      #model_mllib_tree = DecisionTree.trainClassifier(trainingData_rdd, numClasses=2, categoricalFeaturesInfo={},
-      #                               impurity='gini', maxDepth=5, maxBins=100)
-      model_mllib_tree = DecisionTree.trainClassifier(trainingData_rdd, numClasses=2, categoricalFeaturesInfo={},
-                                     impurity='gini', maxDepth=5)
+      if 'Tree_mllib' in model_list:
+        t0=datetime.datetime.now()
 
-      mllib_model_accuracy('Tree',model_mllib_tree,trainingData_rdd,testData_rdd)
+        # Only change parameters here
+        tree_mllib_par_dict={}
+        tree_mllib_par_dict['numClasses'] = 2
+        tree_mllib_par_dict['impurity'] = 'gini'
+        tree_mllib_par_dict['maxDepth'] = 10
 
-      runtime_write('Tree_mllib',t0)
+        model_mllib_tree = DecisionTree.trainClassifier(trainingData_rdd, numClasses=tree_mllib_par_dict['numClasses'], categoricalFeaturesInfo={},
+                                     impurity=tree_mllib_par_dict['impurity'], maxDepth=tree_mllib_par_dict['maxDepth'])
+
+        mllib_model_accuracy('Tree',model_mllib_tree,trainingData_rdd,testData_rdd)
+
+        modelparameter_dict['Tree_mllib']=tree_mllib_par_dict
+
+        runtime_write('Tree_mllib',t0)
+
       ###############################################################################
       # Random forest
-      t0=datetime.datetime.now()
-      #model_mllib_RF = RandomForest.trainClassifier(trainingData_rdd, numClasses=2,
-      #                                   categoricalFeaturesInfo={},
-      #                                   numTrees=10, featureSubsetStrategy="auto",
-      #                                   impurity='gini', maxDepth=5, maxBins=100)
+      if 'RF_mllib' in model_list:
+        t0=datetime.datetime.now()
 
-      model_mllib_RF = RandomForest.trainClassifier(trainingData_rdd, numClasses=2,
+        # Only change parameters here
+        RF_mllib_par_dict={}
+        RF_mllib_par_dict['numClasses'] = 2
+        RF_mllib_par_dict['impurity'] = 'gini'
+        RF_mllib_par_dict['maxDepth'] = 10
+        RF_mllib_par_dict['numTrees'] = 40
+        RF_mllib_par_dict['maxBins'] = 100
+        RF_mllib_par_dict['featureSubsetStrategy'] = 'all'
+
+        model_mllib_RF = RandomForest.trainClassifier(trainingData_rdd, numClasses=RF_mllib_par_dict['numClasses'],
                                          categoricalFeaturesInfo={},
-                                         numTrees=10, featureSubsetStrategy="auto",
-                                         impurity='gini', maxDepth=5)
+                                         maxBins=RF_mllib_par_dict['maxBins'],
+                                         numTrees=RF_mllib_par_dict['numTrees'], featureSubsetStrategy=RF_mllib_par_dict['featureSubsetStrategy'],
+                                         impurity=RF_mllib_par_dict['impurity'], maxDepth=RF_mllib_par_dict['maxDepth'])
 
-      mllib_model_accuracy('RF',model_mllib_RF,trainingData_rdd,testData_rdd)
+        mllib_model_accuracy('RF',model_mllib_RF,trainingData_rdd,testData_rdd)
 
-      runtime_write('RF_mllib',t0)
+        modelparameter_dict['RF_mllib']=RF_mllib_par_dict
+
+        runtime_write('RF_mllib',t0)
+
       ###############################################################################
       # Gradient boosted trees
-      t0=datetime.datetime.now()
-      #model_mllib_GBT = GradientBoostedTrees.trainClassifier(trainingData_rdd, categoricalFeaturesInfo={},learningRate=0.1, loss="logLoss",
-      #                                                       numIterations=50, maxDepth=10)
-      #model_mllib_GBT = GradientBoostedTrees.trainClassifier(trainingData_rdd, categoricalFeaturesInfo={},learningRate=0.1,
-      #                                                       numIterations=10, maxDepth=5)
+      if 'GBT_mllib' in model_list:
+        t0=datetime.datetime.now()
 
-      #mllib_model_accuracy('GBT',model_mllib_GBT,trainingData_rdd,testData_rdd)
+        # Only change parameters here
+        GBT_mllib_par_dict={}
+        #GBT_mllib_par_dict['numClasses'] = 2
+        GBT_mllib_par_dict['loss'] = 'logLoss'
+        GBT_mllib_par_dict['maxDepth'] = 10
+        GBT_mllib_par_dict['numIterations'] = 40
+        GBT_mllib_par_dict['learningRate'] = 0.1
 
-      runtime_write('GBT_mllib',t0)
+        model_mllib_GBT = GradientBoostedTrees.trainClassifier(trainingData_rdd, categoricalFeaturesInfo={},
+                                                        learningRate=GBT_mllib_par_dict['learningRate'], loss=GBT_mllib_par_dict['loss'],
+                                                    numIterations=GBT_mllib_par_dict['numIterations'], maxDepth=GBT_mllib_par_dict['maxDepth'])
+
+        mllib_model_accuracy('GBT',model_mllib_GBT,trainingData_rdd,testData_rdd)
+
+        modelparameter_dict['GBT_mllib']=GBT_mllib_par_dict
+
+        runtime_write('GBT_mllib',t0)
+
       ###############################################################################
       # Logistic regression
-      t0=datetime.datetime.now()
-      #classmethod train(data, iterations=100, step=1.0, miniBatchFraction=1.0, initialWeights=None, 
-      #regParam=0.01, regType='l2', intercept=False, validateData=True)
+      if 'logreg_mllib' in model_list:
+        t0=datetime.datetime.now()
+        #classmethod train(data, iterations=100, step=1.0, miniBatchFraction=1.0, initialWeights=None, 
+        #regParam=0.01, regType='l2', intercept=False, validateData=True)
 
-      iterations=100
-      #model_mllib_logreg = LogisticRegressionWithSGD.train(trainingData_rdd,iterations,step=1, miniBatchFraction=1.0, initialWeights=None, 
-      # regParam=1, regType='l2')
+        iterations=100
+        #model_mllib_logreg = LogisticRegressionWithSGD.train(trainingData_rdd,iterations,step=1, miniBatchFraction=1.0, initialWeights=None, 
+        # regParam=1, regType='l2')
 
-      model_mllib_logreg = LogisticRegressionWithLBFGS.train(trainingData_rdd, iterations=200, initialWeights=None, 
+        model_mllib_logreg = LogisticRegressionWithLBFGS.train(trainingData_rdd, iterations=200, initialWeights=None, 
          regParam=0.01, regType='l2', intercept=False, corrections=10, tolerance=0.0001)
 
-      mllib_model_accuracy('logreg',model_mllib_logreg,trainingData_rdd,testData_rdd)
+        mllib_model_accuracy('logreg',model_mllib_logreg,trainingData_rdd,testData_rdd)
 
-      runtime_write('logreg_mllib',t0)
+        runtime_write('logreg_mllib',t0)
       ###############################################################################
 
       ###############################################################################
-      # Support Vector machines (SVM) only linear kernel 
-      t0=datetime.datetime.now()
-      #model_mllib_SVM = SVMWithSGD.train(trainingData_rdd)
-      #model_mllib_SVM = SVMWithSGD.train(trainingData_rdd, iterations)
+      # Support Vector machines (SVM) only linear kernel (dont use now)
+      if 'SVM_mllib' in model_list:
+        t0=datetime.datetime.now()
+        model_mllib_SVM = SVMWithSGD.train(trainingData_rdd)
+        #model_mllib_SVM = SVMWithSGD.train(trainingData_rdd, iterations)
 
-      #mllib_model_accuracy('SVM',model_mllib_SVM,trainingData_rdd,testData_rdd)
+        mllib_model_accuracy('SVM',model_mllib_SVM,trainingData_rdd,testData_rdd)
 
-      runtime_write('SVM_mllib',t0)
+        runtime_write('SVM_mllib',t0)
+
       ###############################################################################
 
       sc.stop()
@@ -388,7 +473,8 @@ if __name__ == "__main__":
         writer=csv.writer(open(f_name,'wb'),delimiter=':')
         for key in sorted(timing_dict.keys()):
           writer.writerow([key, timing_dict[key]])
-        #with open(f_name,'w') as f:
-        #  for key in sorted(metrics_dict.keys()):
-        #    f.write(key+', '+ str(metrics_dict[key])+'\n')
+      f_name='modelparameters'+'.json'
+      with open(f_name,'w') as f:
+        f.write(json.dumps(modelparameter_dict,indent=2))
+
 
